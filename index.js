28a1324969c5dd4cb5eeb8ac19a54f83402ca8e4
LAPTOP-9KDDCU7B\kkkuk <kkuk090@gmail.com>
2021-07-26 23:10:40 +0900

    처음 저장소에 올림

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://JKLee:123qwe@boilerplate.e692x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+  userNewUrlParser: true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
+}).then(() =>console.log('MongoDB Connected...'))
+  .catch(err =>(console.log(err))
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕 소정이~~')
+  res.send('Hello SOSO!~~')
 })
 
 app.listen(port, () => {
