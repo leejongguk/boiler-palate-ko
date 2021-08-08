9c9f8a59e6803510c494679de83b5229c7dbbfeb
LAPTOP-9KDDCU7B\kkkuk <kkuk090@gmail.com>
2021-08-08 22:47:06 +0900

    Authentication -Auth

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,8 +1,11 @@
 import Axios from 'axios';
 import {
     LOGIN_USER,
-    REGISTER_USER
+    REGISTER_USER,
+    AUTH_USER
 } from './types';
+
+
 
 export function loginUser(dataTosubmit){
     const request = Axios.post('/api/users/login',dataTosubmit)
@@ -17,7 +20,6 @@ export function loginUser(dataTosubmit){
 
 export function registerUser(dataTosubmit){
 
-
     const request = Axios.post('/api/users/register',dataTosubmit)
         .then(response => response.data )
         
@@ -27,4 +29,14 @@ export function registerUser(dataTosubmit){
     }
 }
 
+export function auth(){
+
+    const request = Axios.get('/api/users/auth')
+        .then(response => response.data )
+        
+    return {
+        type: AUTH_USER,
+        payload: request
+    }
+}
 
